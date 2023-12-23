2367ce5ea5a29b0d0f1f904b2553f636696f0719
malsblack <sebastian.martinez.lopez98@gmail.com>
2023-12-23 03:49:05 -0600

    Añadido resaltado al nombre de usuario en el correo de bienvenida
    - En la función 'send_registration_email', se aplican estilos CSS en línea para resaltar el nombre de usuario.
    - Modificado 'registro_paciente_post' para incluir el nombre de usuario al enviar el correo de bienvenida.
    - Se añadió validación de correo electrónico y verificación de existencia del correo en 'registro_paciente_post'.
    - Se mejoró la validación de la contraseña, ahora requiere al menos 1 mayúscula, 1 minúscula, 1 número, 1 carácter especial y una longitud mínima de 8 caracteres.

---
+++ PROYECTO/hospital_app/config/utilities.py
@@ -0,0 +1,86 @@
+import json
+import pyodbc
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def credentials_db():
+    with open('./config/config.json', 'r') as archivo:
+        data = json.load(archivo)
+        return f"DRIVER={data['Db']['driver']};SERVER={data['Db']['server']};DATABASE={data['Db']['database']};Trusted_Connection=yes;"
+    
+def credentials_email():
+    with open('./config/config.json', 'r') as archivo:
+        data = json.load(archivo)
+        return data['Email']
+    
+def db_connect():
+    try:
+        connection = pyodbc.connect(credentials_db())
+        print("Conexión exitosa a la base de datos")
+        return connection
+    except pyodbc.Error as ex:
+        # Si hay un error, imprime el mensaje de error
+        print("Error al conectar a la base de datos:", ex)
+        return None
+    
+
+
+def send_registration_email(nombre, ap_pat, ap_mat, contacto, nombre_usuario):
+    email = credentials_email()
+
+    # Crear el objeto del mensaje
+    msg = MIMEMultipart()
+    msg['From'] = email['EMAIL_USERNAME']
+    msg['To'] = contacto
+    msg['Subject'] = '¡Bienvenido a nuestro servicio de salud!'
+
+    # Cuerpo del mensaje con formato HTML
+    body = f'''
+    <html>
+    <head>
+        <style>
+            body {{
+                background-color: #f4f4f4; /* Color de fondo */
+                font-family: 'Arial', sans-serif;
+            }}
+            .container {{
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }}
+            h2 {{
+                color: #007bff;
+            }}
+            p {{
+                margin-bottom: 15px;
+            }}
+            .highlight {{
+                background-color: #ffff66; /* Color de resaltado */
+                font-weight: bold;
+            }}
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h2>¡Bienvenido, {nombre} {ap_pat} {ap_mat}!</h2>
+            <p>
+                Te damos la bienvenida a nuestro servicio de salud. Tu registro ha sido exitoso y ahora eres parte de nuestra comunidad.
+                Esperamos que encuentres la información y los servicios que ofrecemos útiles para tu bienestar.
+            </p>
+            <p>Si tienes alguna pregunta o necesitas asistencia, no dudes en contactarnos.</p>
+            <p>Gracias por confiar en nosotros, tu usuario es : <span class="highlight">{nombre_usuario}</span>. ¡Te deseamos salud y felicidad!</p>
+        </div>
+    </body>
+    </html>
+    '''
+    msg.attach(MIMEText(body, 'html'))
+
+    # Conectar al servidor SMTP y enviar el correo
+    with smtplib.SMTP(email['EMAIL_HOST'], email['EMAIL_PORT']) as server:
+        server.starttls()
+        server.login(email['EMAIL_USERNAME'], email['EMAIL_PASSWORD'])
+        server.sendmail(email['EMAIL_USERNAME'], contacto, msg.as_string())
--- PROYECTO/hospital_app/main.py
+++ PROYECTO/hospital_app/main.py
@@ -1,30 +1,134 @@
 from flask import Flask, render_template, request , redirect , flash, session
 import pyodbc
 from datetime import datetime
+import re
+from config.utilities import db_connect,send_registration_email
 
 app = Flask(__name__)
 app.secret_key = 'ProyectoDSBT'
-
-
-# Configuración de la cadena de conexión para autenticación de Windows
-server = 'LAPTOP-3KA9U966'  # Reemplaza con tu servidor SQL Server
-database = 'ProyectoDBST'  # Reemplaza con el nombre de tu base de datos
-driver = '{ODBC Driver 17 for SQL Server}'
-
-# Cadena de conexión completa para autenticación de Windows
-connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection=yes;'
-
-try:
-    # Intenta establecer la conexión
-    connection = pyodbc.connect(connection_string)
-    print("Conexión exitosa a la base de datos")
-except pyodbc.Error as ex:
-    # Si hay un error, imprime el mensaje de error
-    print("Error al conectar a la base de datos:", ex)
-    connection = None
-
-# Crear un cursor para ejecutar consultas SQL
+connection=db_connect()
 cursor = connection.cursor()
+
+#---------------------------- Index ------------------------------------------------------
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#---------------------------- Paciente ----------------------------------------------------
+
+@app.route('/login_paciente')
+def login_paciente():
+    return render_template('login_paciente.html')
+
+@app.route('/login_paciente_post',methods = ['POST'])
+def login_paciente_post():
+    if request.method == 'POST':
+        # Obtener los datos del formulario de inicio de sesión
+        id_paciente = request.form['id_paciente']
+        contraseña = request.form['contrasena']
+
+        try:
+            # Verificar las credenciales en la base de datos
+            cursor.execute("SELECT ID_Paciente FROM Paciente WHERE ID_Paciente = ? AND Contraseña = ?", id_paciente, contraseña)
+            result = cursor.fetchone()
+
+            if result:
+                # Si las credenciales son correctas, almacenar el ID en la variable de sesión
+                session['id_paciente'] = id_paciente
+
+                # Redirigir a la página del paciente
+                return redirect('/dashboard_paciente')
+            else:
+                # Si las credenciales son incorrectas, mostrar un mensaje de error
+                flash("Credenciales incorrectas. Inténtalo de nuevo.", 'error')
+                return redirect('/dashboard_paciente')
+
+        except pyodbc.Error as ex:
+            # Si hay un error, imprimir el mensaje
+            print("Error al iniciar sesión del paciente:", ex)
+
+    # Redirigir a la página del paciente en cualquier caso
+    return redirect('/login_paciente')
+
+@app.route('/registro_paciente')
+def registro_paciente():
+    return render_template('registro_paciente.html')
+
+@app.route('/registro_paciente_post', methods=['POST'])
+def registro_paciente_post():
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        nombre = request.form['nombre']
+        ap_pat = request.form['ap_pat']
+        ap_mat = request.form['ap_mat']
+        contacto = request.form['contacto']
+        fecha_nac = request.form['fecha_nac']
+        sexo = request.form['sexo']
+        contraseña = request.form['password']
+        TipoTabla = "Paciente"
+
+        # Validar que el campo de contacto sea un correo electrónico válido
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", contacto):
+            flash("Por favor, introduce un correo electrónico válido.", 'error')
+            return redirect('/registro_paciente')
+
+        # Verificar si el correo ya está registrado
+        cursor.execute("SELECT ID_Paciente FROM Paciente WHERE Contacto = ?", contacto)
+        existing_patient = cursor.fetchone()
+
+        if existing_patient:
+            flash("Este correo electrónico ya está registrado. Por favor, utiliza otro.", 'error')
+            return redirect('/registro_paciente')
+
+        # Validar la contraseña
+        if not re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", contraseña):
+            flash("La contraseña debe tener al menos 1 mayúscula, 1 minúscula, 1 número, 1 carácter especial y una longitud mínima de 8 caracteres.", 'error')
+            return redirect('/registro_paciente')
+
+        try:
+            # Convertir la fecha a un formato adecuado para tu base de datos
+            fecha_nac = datetime.strptime(fecha_nac, '%Y-%m-%d').date()
+
+            # Ejecutar el procedimiento almacenado para insertar datos de paciente
+            cursor.execute("EXEC InsertarDatos ?, ?, ?, ?, ?, ?, ?, ?",
+                           nombre, ap_pat, ap_mat, contacto, fecha_nac, sexo, contraseña, TipoTabla)
+
+            # Confirmar la transacción
+            connection.commit()
+
+            # Obtener el ID del nuevo usuario
+            cursor.execute("SELECT ID_Paciente FROM Paciente WHERE Nombre = ? AND Ap_Pat = ? AND Ap_Mat = ?",
+                           nombre, ap_pat, ap_mat)
+            id_paciente = cursor.fetchone()[0]
+
+            # Almacenar el ID en la variable de sesión
+            session['id_paciente'] = id_paciente
+
+            send_registration_email(nombre, ap_pat, ap_mat, contacto,id_paciente)
+
+            # Mostrar mensaje flash y redirigir
+            flash(f"Registro de paciente exitoso. ID: {id_paciente}", 'success')
+            return redirect('/login_paciente')
+
+        except pyodbc.Error as ex:
+            # Si hay un error, imprimir el mensaje y hacer rollback
+            print("Error al registrar paciente:", ex)
+            flash("Error al registrar paciente. Inténtalo de nuevo.", 'error')
+            connection.rollback()
+
+    # Si el método no es POST o hay un error, redirigir a la página de registro del paciente
+    return redirect('/registro_paciente')  
+@app.route('/dashboard_paciente')
+
+
+def dashboard_paciente():
+    # Verificar si el paciente ha iniciado sesión
+    if 'id_paciente' not in session:
+        flash("Debes iniciar sesión para acceder a la consola del paciente.", 'error')
+        return redirect('/login_paciente')
+
+    return render_template('dashboard_paciente.html')   
+
 
 @app.route('/agendar_cita_post', methods=['POST'])
 def agendar_cita_post():
@@ -61,95 +165,17 @@ def agendar_cita():
     return render_template('agendar_cita.html')
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/paciente')
-def paciente():
-    return render_template('paciente.html')
-
-@app.route('/paciente_post',methods = ['POST'])
-def paciente_post():
-    if request.method == 'POST':
-        # Obtener los datos del formulario de inicio de sesión
-        id_paciente = request.form['id_paciente']
-        contraseña = request.form['contrasena']
-
-        try:
-            # Verificar las credenciales en la base de datos
-            cursor.execute("SELECT ID_Paciente FROM Paciente WHERE ID_Paciente = ? AND Contraseña = ?", id_paciente, contraseña)
-            result = cursor.fetchone()
-
-            if result:
-                # Si las credenciales son correctas, almacenar el ID en la variable de sesión
-                session['id_paciente'] = id_paciente
-
-                # Redirigir a la página del paciente
-                return redirect('/consola_paciente')
-            else:
-                # Si las credenciales son incorrectas, mostrar un mensaje de error
-                flash("Credenciales incorrectas. Inténtalo de nuevo.", 'error')
-                return redirect('/paciente')
-
-        except pyodbc.Error as ex:
-            # Si hay un error, imprimir el mensaje
-            print("Error al iniciar sesión del paciente:", ex)
-
-    # Redirigir a la página del paciente en cualquier caso
-    return redirect('/paciente')
-
-@app.route('/registro_paciente_post' ,methods = ['POST'])
-def registro_paciente_post():
-    print(request.form)
-    if request.method == 'POST':
-        # Obtener los datos del formulario
-        nombre = request.form['nombre']
-        ap_pat = request.form['ap_pat']
-        ap_mat = request.form['ap_mat']
-        contacto = request.form['contacto']
-        fecha_nac = request.form['fecha_nac']
-        sexo = request.form['sexo']
-        contraseña = request.form['password']
-        TipoTabla="Medico"
-
-        try:
-            # Ejecutar el procedimiento almacenado para insertar datos de paciente
-            cursor.execute("EXEC InsertarDatos ?, ?, ?, ?, ?, ?, ?, ?",
-                           nombre, ap_pat, ap_mat, contacto, fecha_nac, sexo, contraseña, TipoTabla)
-            # Confirmar la transacción
-            connection.commit()
-             # Obtener el ID del nuevo usuario
-            cursor.execute("SELECT ID_Paciente FROM Paciente WHERE Nombre = ? AND Ap_Pat = ? AND Ap_Mat = ?",
-                           nombre, ap_pat, ap_mat)
-            id_paciente = cursor.fetchone()[0]
-
-
-            # Almacenar el ID en la variable de sesión
-            session['id_paciente'] = id_paciente
-
-            # Mostrar mensaje flash y redirigir
-            flash(f"Registro de paciente exitoso. ID: {id_paciente}", 'success')
-            return redirect('/paciente')
+       
             
-        except pyodbc.Error as ex:
-            # Si hay un error, imprimir el mensaje y hacer rollback
-            print("Error al registrar paciente:", ex)
-            connection.rollback()
-  
-@app.route('/registro_paciente')
-def registro_paciente():
-    return render_template('registro_paciente.html')
+#---------------------------- Medico ----------------------------------------------------
+#---------------------------- Farmacia ----------------------------------------------------
+#---------------------------- Recepcion ----------------------------------------------------
 
-@app.route('/consola_paciente')
-def consola_paciente():
-    # Verificar si el paciente ha iniciado sesión
-    if 'id_paciente' not in session:
-        flash("Debes iniciar sesión para acceder a la consola del paciente.", 'error')
-        return redirect('/paciente')
 
-    return render_template('consola_paciente.html')          
-            
+
+
+
 
 
 @app.route('/recepcion')
